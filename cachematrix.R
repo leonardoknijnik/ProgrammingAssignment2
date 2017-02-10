4f51d280ae40a88265272ec7bb9f71cb7dc0d614
leonardoknijnik <leonardomeesknijnik@gmail.com>
2017-02-10 14:55:24 -0200

    Add files via upload
    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function is responsibe for storing (cache) the values obatined in the CasheSolve function. 
+# That is why we need to use the "<<-" operator, so that they share an environment and the
+# Functions are able to read their common variables, such as "inv"
 
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL   # null (in case CacheSolve has not been used)
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x           
+  setmatrix <- function(inverse) inv <<- inverse
+  getmatrix <- function() inv   #getmatrix is the variable in which the inverse is going to
+  list(set = set, get = get,    # be stored in, and it is retrieved by the solveMatrix function
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function obtains the inverse of the argument matrix either from solving it
+## or retrieving from the cached value in makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getmatrix()
+  if(!is.null(inv)) {   #if inv is not NULL (if there is already a stored value in MakeCacheMatrix)
+    message("getting cached data")    #the function simply returns the cached result
+    return(inv)
+  }
+  mat <- x$get()                     # if inv is null, cacheSolve obatains the inverse of
+  inv <- solve(mat, ...)             # the matrix x and caches it in the makeCacheMatrix
+  x$setmatrix(inv)
+  inv
+      
 }
